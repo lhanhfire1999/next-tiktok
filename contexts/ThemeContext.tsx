3936bc7bddb6b40df78cc7e80b5c23e09d417fbe
Anh Le <lhanhfire1999@gmail.com>
2022-12-25 03:08:00 +0700

    fix(theme-context): solve hydration error

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { createContext, useContext, useState } from 'react'
-import useIsomorphicLayoutEffect from '~/hooks/useIsomorphicLayoutEffect'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
 
 enum MODE_THEME {
   LIGHT = 'light',
@@ -19,16 +18,8 @@ interface ThemeProvider {
 const Context = createContext<ContextProp | null>(null)
 
 export const ThemeProvider: React.FC<ThemeProvider> = ({ children }) => {
-  const [theme, setTheme] = useState(() => {
-    if (typeof window !== 'undefined') {
-      const themeStorage = localStorage!.getItem('theme')
-      const preferDarkSchema = window!.matchMedia && window!.matchMedia('(prefers-color-scheme: dark)').matches
-      const defaultTheme = preferDarkSchema ? MODE_THEME.DARK : MODE_THEME.LIGHT
-
-      return themeStorage ?? defaultTheme
-    }
-    return MODE_THEME.LIGHT
-  })
+  const [theme, setTheme] = useState(MODE_THEME.LIGHT)
+  const isFirstRenderRef = useRef(true)
 
   const handleChangeTheme = () => {
     setTheme((prev) => {
@@ -37,7 +28,21 @@ export const ThemeProvider: React.FC<ThemeProvider> = ({ children }) => {
     })
   }
 
-  useIsomorphicLayoutEffect(() => {
+  useEffect(() => {
+    if (isFirstRenderRef.current) {
+      const themeStorage = localStorage!.getItem('theme')
+      const preferDarkSchema = window!.matchMedia && window!.matchMedia('(prefers-color-scheme: dark)').matches
+      const defaultTheme = themeStorage || preferDarkSchema ? MODE_THEME.DARK : MODE_THEME.LIGHT
+
+      if (defaultTheme !== theme) {
+        document.documentElement.setAttribute('data-theme', defaultTheme)
+        localStorage.setItem('theme', defaultTheme)
+        setTheme(defaultTheme)
+      }
+      isFirstRenderRef.current = false
+      return
+    }
+
     document.documentElement.setAttribute('data-theme', theme)
     localStorage.setItem('theme', theme)
   }, [theme])
